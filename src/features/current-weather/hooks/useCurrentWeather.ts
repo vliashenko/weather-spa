59aebf4ef86edb8ed2fb21df4e496a07ba836e34
Liashenko <greedisdood1@gmail.com>
2025-07-20 15:08:36 +0300

    add env var to config

--- src/features/current-weather/hooks/useCurrentWeather.ts
+++ src/features/current-weather/hooks/useCurrentWeather.ts
@@ -1,30 +1,13 @@
 import { useEffect } from 'react';
 import { useAppDispatch } from '../../../stores';
-import { getCurrentWeatherByCity, getCurrentWeatherByGeo, getSavedCities } from '../services';
+import { getCurrentWeatherByCity, getSavedCities } from '../services';
 import type { City } from '../types';
 
 export const useCurrentWeather = (city?: City) => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const geo = navigator.geolocation;
-
-    if (geo) {
-      geo.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          console.log(latitude, longitude);
-          dispatch(getCurrentWeatherByGeo({ lat: latitude, lon: longitude }));
-        },
-        (error) => {
-          console.warn('Geolocation error:', error);
-          dispatch(getCurrentWeatherByCity(city ?? 'Kyiv'));
-          dispatch(getSavedCities());
-        }
-      );
-    } else {
-      dispatch(getCurrentWeatherByCity(city ?? 'Kyiv'));
-      dispatch(getSavedCities());
-    }
+    dispatch(getCurrentWeatherByCity(city ?? 'Kyiv'));
+    dispatch(getSavedCities());
   }, [city, dispatch]);
 };
