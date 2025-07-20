01ff55d30772a37d05b57616d2ff69839dce592f
Liashenko <greedisdood1@gmail.com>
2025-07-20 15:16:03 +0300

    add env var to config

--- src/features/current-weather/hooks/useCurrentWeather.ts
+++ src/features/current-weather/hooks/useCurrentWeather.ts
@@ -1,13 +1,33 @@
 import { useEffect } from 'react';
 import { useAppDispatch } from '../../../stores';
-import { getCurrentWeatherByCity, getSavedCities } from '../services';
+import { getCurrentWeatherByCity, getCurrentWeatherByGeo, getSavedCities } from '../services';
 import type { City } from '../types';
 
 export const useCurrentWeather = (city?: City) => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getCurrentWeatherByCity(city ?? 'Kyiv'));
-    dispatch(getSavedCities());
+    const geo = navigator.geolocation;
+
+    if (geo) {
+      geo.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          dispatch(getCurrentWeatherByGeo({ lat: latitude, lon: longitude }));
+          dispatch(getSavedCities());
+        },
+        () => {
+          dispatch(getCurrentWeatherByCity(city ?? 'Kyiv'));
+          dispatch(getSavedCities());
+        },
+        {
+          timeout: 5000,
+          maximumAge: 0
+        }
+      );
+    } else {
+      dispatch(getCurrentWeatherByCity(city ?? 'Kyiv'));
+      dispatch(getSavedCities());
+    }
   }, [city, dispatch]);
 };
